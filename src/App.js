d2cf9df60fd6a8f232244989131374f100813796
mohit <mohitklamba@gmail.com>
2021-03-11 22:36:02 +0530

    option added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React from "react";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-    <h1>Lets build a covid tracker</h1>
+    <h1>COVID-TRACKER-19</h1>
+    <FormControl class="app__dropdown">
+    <Select variant="outlined" value="abc">
+    <MenuItem value="worldwide">Worldwide</MenuItem>
+    <MenuItem value="worldwide">Option two</MenuItem>
+    <MenuItem value="worldwide">Option 3</MenuItem>
+    <MenuItem value="worldwide">YOOOOOO</MenuItem>
+     
+    
+    </Select>
+    
+    </FormControl>
   {/* Header */}
   {/* Title + Select input dropdown field */}
   {/* InfoBox */}
