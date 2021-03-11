b9765695a708a5f0d7d330d1c67863d88da1bd98
mohit <mohitklamba@gmail.com>
2021-03-11 23:05:47 +0530

    state added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   MenuItem,
   FormControl,
@@ -7,20 +7,31 @@ import {
 import './App.css';
 
 function App() {
+  const [countries,setCountries]=useState([
+    'USA','UK','INDIA'
+  ]);
   return (
     <div className="app">
+    <div className="app__header">
     <h1>COVID-TRACKER-19</h1>
     <FormControl class="app__dropdown">
     <Select variant="outlined" value="abc">
-    <MenuItem value="worldwide">Worldwide</MenuItem>
+    {
+      countries.map(country=> (
+        <MenuItem value={country}>{country}</MenuItem>
+      ))
+    }
+   {/* <MenuItem value="worldwide">Worldwide</MenuItem>
     <MenuItem value="worldwide">Option two</MenuItem>
     <MenuItem value="worldwide">Option 3</MenuItem>
     <MenuItem value="worldwide">YOOOOOO</MenuItem>
-     
+  */}
     
     </Select>
     
     </FormControl>
+   
+    </div>
   {/* Header */}
   {/* Title + Select input dropdown field */}
   {/* InfoBox */}
