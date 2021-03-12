ebd787ad94e9c8ed8e14b68b4064085ce055abe1
mohit <mohitklamba@gmail.com>
2021-03-12 15:51:06 +0530

    added countries

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   MenuItem,
   FormControl,
@@ -10,15 +10,45 @@ function App() {
   const [countries,setCountries]=useState([
     'USA','UK','INDIA'
   ]);
+  // STATE= how to write a variable in react <<<<<<< 
+
+  // https://disease.sh/v3/covid-19/countries
+
+  // USEEFFECT= runs a piece of code 
+  // based on a given condition
+
+  useEffect(()=> {
+    // the code inside here will run once 
+    // when the component loads and not again
+    // async -> send a request,wait for it , do something with it
+
+    const getCountriesData = async() => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data)=> {
+        const countries= data.map((country) => (
+          {
+            name:country.country,
+            value:country.countryInfo.iso2
+          }));
+          setCountries(countries);
+      });
+
+    };
+
+    getCountriesData();
+
+  },[]);
   return (
     <div className="app">
     <div className="app__header">
     <h1>COVID-TRACKER-19</h1>
     <FormControl class="app__dropdown">
     <Select variant="outlined" value="abc">
+    {/*Loop through all the countries and show a dropdown list of all options */}
     {
       countries.map(country=> (
-        <MenuItem value={country}>{country}</MenuItem>
+        <MenuItem value={country.value}>{country.name}</MenuItem>
       ))
     }
    {/* <MenuItem value="worldwide">Worldwide</MenuItem>
